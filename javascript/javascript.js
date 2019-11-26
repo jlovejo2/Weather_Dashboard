d0bb1ed0bcd6f2da601b537bb56ec9012c3d2817
James Lovejoy <james.lovejoy2@gmail.com>
2019-11-26 17:54:17 -0600

    completed jquery code for ajax query

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,11 +4,12 @@ var container = $(".container-fluid");
 var userSearchList = $("<ul>");
 
 
+
 //Adding text to header
 h1.text("Weather Map").addClass("header");
 header.append(h1);
 
-container.append($("<div>").addClass("row").attr("id","mainContent"));
+container.append($("<div>").addClass("row").attr("id", "mainContent"));
 
 //make first column
 var firstCol = $("<div>");
@@ -21,10 +22,10 @@ var searchBar1 = $("<div>");
 var searchBar2 = $("<div>");
 searchBar1.addClass("input-group md-form form-1 pl-0");
 searchBar2.addClass("input-group-prepend");
-searchBar2.append($("<span>").attr({"class":"input-group-text cyan lighten-2", "id":"basic-text1"}).append($("<i>").attr({"class":"fa fa-search text-white","aria-hidden":"true"})));
+searchBar2.append($("<span>").attr({ "class": "input-group-text cyan lighten-2", "id": "basic-text1" }).append($("<i>").attr({ "class": "fa fa-search text-white", "aria-hidden": "true" })));
 searchBar1.append(searchBar2);
 firstCol.append(searchBar1);
-searchBar1.append($("<input>").attr({"class":"form-control my-0 py-1","type":"text","id":"userInput","placeholder":"Search","aria-label":"Search"}));
+searchBar1.append($("<input>").attr({ "class": "form-control my-0 py-1", "type": "text", "id": "userInput", "placeholder": "Search", "aria-label": "Search" }));
 
 //make ul for user searches to get listed in
 firstCol.append(userSearchList);
@@ -48,13 +49,28 @@ $("#columnTwo").append($("<div>").addClass("row").attr("id", "weatherData"));
 $("#columnTwo").append($("<div>").addClass("row").attr("id", "fiveDayForecast"));
 
 //click event on the container area below jumbotron
-container.click(function() {
-
+container.click(function () {
+    var cityName = $("#userInput").val();
+    console.log(cityName);
     console.log(event);
-    if(event.target.tagName === "I") {
+    var APIKey = "21a347a444e91fd3f7484f44867c287b";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey;
+    
+    if (event.target.tagName === "I") {
         var userSearch = $("<button>");
-       userSearch.text($("#userInput").val());
-       userSearchList.append($("<li>").append(userSearch));
-       
+        userSearch.text(cityName);
+        userSearchList.append($("<li>").append(userSearch));
+
+        //Ajax query is the code that sends the request to the weather API for the data.
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        
+    });
+
     }
+
 });
+
