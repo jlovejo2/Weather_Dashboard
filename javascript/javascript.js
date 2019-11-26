63dd426765b753eb96ec90524124c4b4571e92fc
James Lovejoy <james.lovejoy2@gmail.com>
2019-11-26 16:43:04 -0600

    completed code for search bar generated list of buttons based on user input

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,7 @@
 var h1 = $("h1");
 var header = $("#header");
 var container = $(".container-fluid");
+var userSearchList = $("<ul>");
 
 
 //Adding text to header
@@ -13,18 +14,20 @@ container.append($("<div>").addClass("row").attr("id","mainContent"));
 var firstCol = $("<div>");
 firstCol.addClass("col-lg-4 col-md-4 col-sm-12");
 firstCol.attr("id", "columnOne");
-firstCol.text("first column");
 $("#mainContent").append(firstCol);
 
 //make div for searchbar in first column
 var searchBar1 = $("<div>");
 var searchBar2 = $("<div>");
-searchBar1.addClass("input-group md-form form-sm form-1 pl-0");
+searchBar1.addClass("input-group md-form form-1 pl-0");
 searchBar2.addClass("input-group-prepend");
-searchBar2.append($("<span>").attr({"class":"input-group-text cyan lighten-2", "id":"basic-text1"}).append($("<i>").attr({"class":"fas fa-search text-white","aria-hidden":"true"})));
+searchBar2.append($("<span>").attr({"class":"input-group-text cyan lighten-2", "id":"basic-text1"}).append($("<i>").attr({"class":"fa fa-search text-white","aria-hidden":"true"})));
 searchBar1.append(searchBar2);
 firstCol.append(searchBar1);
-searchBar1.append($("<input>").attr({"class":"form-control my-0 py-1","type":"text","placeholder":"Search","aria-label":"Search"}));
+searchBar1.append($("<input>").attr({"class":"form-control my-0 py-1","type":"text","id":"userInput","placeholder":"Search","aria-label":"Search"}));
+
+//make ul for user searches to get listed in
+firstCol.append(userSearchList);
 
 {/* <div class="input-group md-form form-sm form-1 pl-0">
   <div class="input-group-prepend">
@@ -38,9 +41,20 @@ searchBar1.append($("<input>").attr({"class":"form-control my-0 py-1","type":"te
 var secondCol = $("<div>");
 secondCol.addClass("col-lg-8 col-md-8 col-sm-12");
 secondCol.attr("id", "columnTwo");
-secondCol.text("second column");
 $("#mainContent").append(secondCol);
 
 //add rows to larger column (first column)
 $("#columnTwo").append($("<div>").addClass("row").attr("id", "weatherData"));
 $("#columnTwo").append($("<div>").addClass("row").attr("id", "fiveDayForecast"));
+
+//click event on the container area below jumbotron
+container.click(function() {
+
+    console.log(event);
+    if(event.target.tagName === "I") {
+        var userSearch = $("<button>");
+       userSearch.text($("#userInput").val());
+       userSearchList.append($("<li>").append(userSearch));
+       
+    }
+});
